0dce9ec4436d9a737840078938d9c55dd5f3ecd9
Elías Heresi <elias.heresi@gmail.com>
2022-04-14 18:41:34 +0200

    Corrected initial files

--- skylabConstructor.js
+++ skylabConstructor.js
@@ -1,1 +1,9 @@
-class SkylabArray {}
+function
+
+describe("Given the class SkylabArray", () => {
+  describe("When it receives 1 as parameter", () => {
+    test("Then it should return the array [1]", () => {
+
+    });
+  });
+});
