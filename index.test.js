01a0e94691a2293a467598ce36e8867bdaa2a5b1
Cristian Bermúdez <ccbabcn@gmail.com>
2022-04-15 01:40:28 +0200

    implemented the class SkylabArray, it's method myArrayLenght and test

--- index.test.js
+++ index.test.js
@@ -1,19 +1,39 @@
-const SkylabArray = (input) => {
-  const newArray = [input];
-  return newArray;
-};
+class SkylabArray {
+  constructor(...theArgs) {
+    this.myArray = theArgs;
+  }
+
+  myArrayLenght() {
+    let myCount = 0;
+    for (this.Object in this.myArray) {
+      if (this.myArray) {
+        myCount += 1;
+      }
+    }
+    return myCount;
+  }
+}
 
 describe("Given the class SkylabArray", () => {
-  describe("When it receives 1 as parameter", () => {
-    test("Then it should return the array [1]", () => {
-      const inputtedNumber = 1;
-      const expectedResult = [1];
+  describe("When it receives the arguments 3, 8, 10, 2 to create the Object numbers", () => {
+    test("Then it's method myArrayLenght should return 4", () => {
+      const numbers = new SkylabArray(3, 8, 10, 2);
+      const expectedLength = 4;
 
-      const testedResult = SkylabArray(inputtedNumber);
+      const ArrayLenghtResult = numbers.myArrayLenght();
 
-      expect(expectedResult).toBe(testedResult);
+      expect(ArrayLenghtResult).toBe(expectedLength);
+    });
+  });
+
+  describe("When it receives the arguments 'Marta', 'Luis', 'Pere' to create the Object names", () => {
+    test("Then it's method myArrayLenght should return 4", () => {
+      const names = new SkylabArray("Marta", "Luis", "Pere");
+      const expectedLength = 3;
+
+      const ArrayLenghtResult = names.myArrayLenght();
+
+      expect(ArrayLenghtResult).toBe(expectedLength);
     });
   });
 });
-
-const a = 3;
